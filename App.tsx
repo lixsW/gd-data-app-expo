07f9b7cd909c9ea0c53264ca4ff105c4e517f4c2
Lixs <835249823@qq.com>
2023-12-26 13:31:59 +0800

    删除外部资源总文件夹

--- App.tsx
+++ App.tsx
@@ -5,24 +5,24 @@ import {
   StatusBar,
   Platform,
   SafeAreaView,
-} from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import BottomTabBar from "./src/routers";
-import "./src/utils/Storage.ts";
-import Constants from "expo-constants";
+} from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import BottomTabBar from './src/routers'
+import './src/utils/Storage.ts'
+import Constants from 'expo-constants'
 
 export default function App() {
   storage.save({
-    key: "token",
-    data: "eyJhbGciOiJIUzUxMiJ9.eyJ1c2VyaWQiOjEsInVzZXJuYW1lIjoiYWRtaW4iLCJuaWNrbmFtZSI6IumrmOeJueeUteWtkCIsImNvbXBhbnljb2RlIjoiZ29sZCIsInRlbmFudGlkIjo5OTk5LCJkc2tleSI6Im1hc3RlciIsInBsYXRmb3JtdXNlcmlkIjoxLCJwbGF0Zm9ybXVzZXJuYW1lIjoiYWRtaW4iLCJwZXJtaXNzaW9uc1JlZGlzS2V5IjoicGVybWlzc2lvbjoxIiwicm9sZXMiOlsiYWRtaW4iXSwidG9rZW5SZWRpc0tleSI6ImxvZ2luX3Rva2VuczplYjlmNGE5MS03MWRkLTQyNjYtYWM0OS0zNTliY2RmYjJhYTYifQ.jwaV4zpsR2T8qlKuFPZsnK1jQgkyaYgU2AJIEH94to2-kL2gouQ1DYLiuYkTppfZm9EzK_Aa_r8VKQLbMEiFlQ",
-  });
+    key: 'token',
+    data: 'eyJhbGciOiJIUzUxMiJ9.eyJ1c2VyaWQiOjEsInVzZXJuYW1lIjoiYWRtaW4iLCJuaWNrbmFtZSI6IumrmOeJueeUteWtkCIsImNvbXBhbnljb2RlIjoiZ29sZCIsInRlbmFudGlkIjo5OTk5LCJkc2tleSI6Im1hc3RlciIsInBsYXRmb3JtdXNlcmlkIjoxLCJwbGF0Zm9ybXVzZXJuYW1lIjoiYWRtaW4iLCJwZXJtaXNzaW9uc1JlZGlzS2V5IjoicGVybWlzc2lvbjoxIiwicm9sZXMiOlsiYWRtaW4iXSwidG9rZW5SZWRpc0tleSI6ImxvZ2luX3Rva2VuczplYjlmNGE5MS03MWRkLTQyNjYtYWM0OS0zNTliY2RmYjJhYTYifQ.jwaV4zpsR2T8qlKuFPZsnK1jQgkyaYgU2AJIEH94to2-kL2gouQ1DYLiuYkTppfZm9EzK_Aa_r8VKQLbMEiFlQ',
+  })
 
   return (
     <SafeAreaView
       style={[
         styles.app,
         {
-          paddingTop: Platform.OS === "android" ? Constants.statusBarHeight : 0,
+          paddingTop: Platform.OS === 'android' ? Constants.statusBarHeight : 0,
         },
       ]}
     >
@@ -31,19 +31,19 @@ export default function App() {
         translucent={true}
         networkActivityIndicatorVisible={true}
         animated={true}
-        barStyle={"light-content"}
-        backgroundColor={"#0f1529"}
+        barStyle={'light-content'}
+        backgroundColor={'#0f1529'}
       />
       <NavigationContainer>
         <BottomTabBar />
       </NavigationContainer>
     </SafeAreaView>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   app: {
     flex: 1,
-    backgroundColor: "#0f1529",
+    backgroundColor: '#0f1529',
   },
-});
+})
